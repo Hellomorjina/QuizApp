23ee5d588bca96df03013c05e24163e646c1c3b3
morjina <fmorjina111@gmail.com>
2025-01-24 15:34:11 +0600

    Add New

--- src/components/Quize.jsx
+++ src/components/Quize.jsx
@@ -64,6 +64,7 @@ const Quize = () => {
   return (
     <div className='container'>
        <h1>Quiz App</h1>
+     
        <hr/>
        {result?<></>:<>
             <h2> {index+1}.{question.question}</h2>
